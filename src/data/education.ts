c294c4e5f49cef6f2b3277c9946fbee4db44fe19
boqian-li <boqianlihuster@gmail.com>
2025-08-06 23:29:52 -0500

    change section-order

--- src/data/education.ts
+++ src/data/education.ts
@@ -15,12 +15,12 @@ export const educationData: Education[] = [
   //   degree: "Ph.D. in Computer Science",
   //   advisor: "Prof. Sarah Johnson",
   // },
-  {
-    year: "2020—2025",
-    institution: "Huazhong University of Science and Technology(HUST)",
-    degree: "B.Eng. in Electrical Engineering (Biomedical Engineering)",
-    // thesis: "Algorithmic Approaches to Causal Discovery",
-    // Optional links to thesis
-    // thesisUrl: "https://dspace.mit.edu/handle/1721.1/149111"
-  },
+  // {
+  //   year: "2020—2025",
+  //   institution: "Huazhong University of Science and Technology(HUST)",
+  //   degree: "B.Eng. in Electrical Engineering (Biomedical Engineering)",
+  //   // thesis: "Algorithmic Approaches to Causal Discovery",
+  //   // Optional links to thesis
+  //   // thesisUrl: "https://dspace.mit.edu/handle/1721.1/149111"
+  // },
 ];
